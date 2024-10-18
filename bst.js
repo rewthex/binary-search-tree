849243217789372e94a8a4e9ff771dcf087a47a0
Aaron <trapp.root@gmail.com>
2024-10-17 18:45:49 -0700

    Completed find and levelOrder

--- bst.js
+++ bst.js
@@ -27,6 +27,56 @@ export class BinarySearchTree {
 		root.right = this.buildTree(array.slice(mid + 1));
 		return root;
 	}
+
+	insert(value, root = this.root) {
+		if (root === null) {
+			root = new Node(value);
+			return;
+		}
+		if (root.data > value) {
+			if (root.left === null) {
+				root.left = new Node(value);
+				return;
+			} else {
+				this.insert(value, root.left);
+			}
+		}
+		if (root.data < value) {
+			if (root.right === null) {
+				root.right = new Node(value);
+				return;
+			} else {
+				this.insert(value, root.right);
+			}
+		}
+	}
+
+	deleteItem(value, root = this.root) {}
+
+	find(value, root = this.root) {
+		if (root === null || root.data === value) return root;
+		if (value > root.data) {
+			return this.find(value, root.right);
+		}
+		if (value < root.data) {
+			return this.find(value, root.left);
+		}
+	}
+
+	levelOrder(callback) {
+		if (this.root === null) return;
+		let queue = [this.root];
+		while (queue.length > 0) {
+			let current = queue.pop();
+			callback(current);
+			if (current.left) queue.unshift(current.left);
+			if (current.right) queue.unshift(current.right);
+		}
+	}
 }
 
-const bst = new BinarySearchTree([1, 2, 3, 4, 5]);
+const bst = new BinarySearchTree([1, 2, 3, 4, 5, 6]);
+const printNode = (node) => {
+	console.log(node.data);
+};
+bst.levelOrder(printNode)
